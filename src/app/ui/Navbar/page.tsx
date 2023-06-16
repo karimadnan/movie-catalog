98d3e9fa2c13d3dd7370e0d8c9e611670ddc32d9
karimmadnan <karim.adnan@halan.com>
2023-06-16 18:35:09 +0300

    ReStyle navbar

--- src/app/ui/Navbar/page.tsx
+++ src/app/ui/Navbar/page.tsx
@@ -1,5 +1,6 @@
 'use client'
 
+import LiveTvIcon from '@mui/icons-material/LiveTv'
 import { Toolbar } from '@mui/material'
 import AppBar from '@mui/material/AppBar'
 import { StyledTypography } from './style'
@@ -8,7 +9,16 @@ export default function Navbar() {
   return (
     <AppBar position="static">
       <Toolbar>
-        <StyledTypography variant="h5" noWrap>
+        <LiveTvIcon color="secondary" fontSize="large" />
+        <StyledTypography
+          variant="h6"
+          noWrap
+          sx={{
+            ml: 2,
+            fontWeight: 700,
+            letterSpacing: '.3rem',
+          }}
+        >
           Movie Catalog
         </StyledTypography>
       </Toolbar>
