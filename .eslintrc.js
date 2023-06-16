c8ddbd9f3820df3b794001a0774db1b119beb032
karimmadnan <karim.adnan@halan.com>
2023-06-16 18:34:45 +0300

    Update: ESlint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -21,6 +21,7 @@ module.exports = {
   },
   plugins: ['react', 'prettier'],
   rules: {
+    '@typescript-eslint/restrict-template-expressions': 'off',
     '@typescript-eslint/triple-slash-reference': 'off',
     '@typescript-eslint/promise-function-async': 'off',
     'import/prefer-default-export': 'off',
