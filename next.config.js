970306561557baf76e4c0ed3c742c1336012eb50
karimmadnan <karim.adnan@halan.com>
2023-06-18 21:03:40 +0300

    config: next image api url

--- next.config.js
+++ next.config.js
@@ -6,6 +6,16 @@ const nextConfig = {
     API_BASE_URL: process.env.API_BASE_URL,
     API_IMAGE_BASE_URL: process.env.API_IMAGE_BASE_URL,
   },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'image.tmdb.org',
+        port: '',
+        pathname: '/t/p/**',
+      },
+    ],
+  },
 }
 
 module.exports = nextConfig
